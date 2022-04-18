9de024da0afcdeb21b2ce43d15064f6f189fbcc5
Victor Sousa <victorbrito001@gmail.com>
2022-04-18 08:14:51 -0300

    search with select

--- src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -13,6 +13,7 @@ export class CreateDeliverymanUseCase {
     const deliverymanExist = await prisma.deliveryman.findFirst({
       where: {
         username: {
+          equals: username,
           mode: "insensitive",
         },
       },
